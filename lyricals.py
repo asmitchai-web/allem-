cfb3bb0f82d10a82803c03638b18a45540865adf
asmitchai-web <asmitch.ai@gmail.com>
2025-11-30 18:47:54 +0530

    Create lyricals.py

---
+++ lyricals.py
@@ -0,0 +1,10 @@
+def generate_full_lyrics(genre="pop", mood="happy"):
+    return f"""
+[Verse 1]
+This is a {mood} {genre} song,
+Flowing gently all night long.
+
+[Chorus]
+We rise, we shine, we find our way,
+Singing our hearts out every day.
+"""
